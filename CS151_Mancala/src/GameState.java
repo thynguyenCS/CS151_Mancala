c7acb10744772d21b992eb92a429dc1f471be25c
Alex Frank <alexfrankcodes@gmail.com>
2018-05-04 10:13:24 -0700

    Update Game State

--- CS151_Mancala/src/GameState.java
+++ CS151_Mancala/src/GameState.java
@@ -1,15 +1,15 @@
 
 public class GameState {
 	private char currentPlayer;
-	private int[] points;
+	private int[] stones;
 	private boolean isModelNull = false;
 	
 	public GameState(DataModel model) {
 		if(model == null) {
 			isModelNull = true;
 		}else {
-			points = model.getPits();
-			currentPlayer = model.getPlayerTurn();
+			sones = model.get().;
+			currentPlayer = model.getCurrentPlayer();
 		}
 	}
 
