35fcab5d6cc68a76ddecd88910ccb201853cbef4
kathynguyen1125 <thynguyen2511@gmail.com>
2018-05-05 13:01:38 -0700

    Update MancalaHoleComponent.java

--- CS151_Mancala/src/MancalaHoleComponent.java
+++ CS151_Mancala/src/MancalaHoleComponent.java
@@ -1,4 +1,14 @@
+import java.awt.Dimension;
+import java.awt.Graphics;
 
-public class MancalaHoleComponent {
-
+public class MancalaHoleComponent extends PitComponent{
+	private int WIDTH = 100, HEIGHT = 100;
+public MancalaHoleComponent(int numStones, BoardLayout bl) {
+	super(numStones, bl);
+	setPreferredSize(new Dimension(WIDTH, HEIGHT));
+	
+}
+public void paintComponent(Graphics g) {
+	super.paintComponent(g);
+}
 }
